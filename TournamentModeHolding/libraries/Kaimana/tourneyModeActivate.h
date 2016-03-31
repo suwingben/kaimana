b1dcc048e09af998cd60dba35a502623590628bb
mightyjr <info@mightyjrmods.com>
2016-03-30 22:48:28 -0400

    Added tournament mode funtionalities.

---
+++ TournamentModeHolding/libraries/Kaimana/tourneyModeActivate.h
@@ -0,0 +1,29 @@
+// tourneymode animation
+
+void tourneyModeActivate(void)
+{
+	#define  T_DELAY      500    // time to OFF between button sets value in miliseconds
+	kaimana.setALL(RED);
+	kaimana.updateALL();
+	delay( T_DELAY );
+	
+	kaimana.setLED(LED_K4, BLACK);
+	kaimana.setLED(LED_P4, BLACK);
+	kaimana.updateALL();
+	delay( T_DELAY );	
+	
+	kaimana.setLED(LED_K3, BLACK);
+	kaimana.setLED(LED_P3, BLACK);
+	kaimana.updateALL();
+	delay( T_DELAY );
+	
+	kaimana.setLED(LED_K2, BLACK);
+	kaimana.setLED(LED_P2, BLACK);
+	kaimana.updateALL();
+	delay( T_DELAY );
+	
+	kaimana.setLED(LED_K1, BLACK);
+	kaimana.setLED(LED_P1, BLACK);
+	kaimana.updateALL();
+	delay( T_DELAY );
+}
---
+++ TournamentModeHolding/libraries/Kaimana/tourneymodeDectivate.h
@@ -0,0 +1,36 @@
+void tourneyModeDeactivate(void)
+{
+	#define  T_DELAY      500    // time to ON between each button set //value in miliseconds
+		
+	kaimana.setLED(LED_K1, RED);
+	kaimana.setLED(LED_P1, RED);
+	kaimana.updateALL();
+	delay( T_DELAY );
+		
+	kaimana.setLED(LED_K2, RED);
+	kaimana.setLED(LED_P2, RED);
+	kaimana.updateALL();
+	delay( T_DELAY );
+	
+	kaimana.setLED(LED_K3, RED);
+	kaimana.setLED(LED_P3, RED);
+	kaimana.updateALL();
+	delay( T_DELAY );
+	
+	kaimana.setLED(LED_K4, RED );
+	kaimana.setLED(LED_P4, RED);
+	kaimana.updateALL();
+	delay( T_DELAY );	
+
+	kaimana.setALL(BLACK);
+	kaimana.updateALL();
+	delay( T_DELAY );
+	
+	kaimana.setALL(RED);
+	kaimana.updateALL();
+	delay( T_DELAY );
+	
+	kaimana.setALL(BLACK);
+	kaimana.updateALL();
+	delay( T_DELAY );
+}
